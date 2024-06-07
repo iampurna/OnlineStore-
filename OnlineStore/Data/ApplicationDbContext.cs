1fbeba905f4128bf53a7fdba32e344fa1b01f152
legion <legion@DESKTOP-I92NV1C>
2024-06-07 12:25:56 +0545

    Add project files.

---
+++ OnlineStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options)
+            :base(options) 
+        {
+
+        }
+        public DbSet<Category>Categories  { get; set; }
+    }
+}
